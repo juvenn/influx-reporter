0473df3a543a6d23605ee72144e421dc2e414a9f
Juvenn Woo <machese@gmail.com>
2018-08-24 17:50:29 +0800

    Rlease 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject influx-reporter "0.1.1"
+(defproject influx-reporter "0.1.2"
   :description "InfluxDB reporter for dropwizard metrics"
   :url "https://github.com/juvenn/influx-reporter"
   :license {:name "Eclipse Public License"
